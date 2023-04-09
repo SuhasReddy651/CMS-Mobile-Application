c83a7dbc3d82e9a391285a9d93c6955c3646e2b0
Surya Suhas Reddy Sathi <suhas@Suhass-MacBook-Pro.local>
2023-04-09 15:17:57 +0530

    Added attendance view page in student portal

--- lib/screens/student/student_attendance.dart
+++ lib/screens/student/student_attendance.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class StuAttenDash extends StatefulWidget {
@@ -126,23 +128,91 @@ class _DailyAttState extends State<DailyAtt> {
   }
 }
 
-class AttReport extends StatelessWidget {
+class AttReport extends StatefulWidget {
   const AttReport({super.key});
 
   @override
+  State<AttReport> createState() => _AttReportState();
+}
+
+class _AttReportState extends State<AttReport> {
+  CollectionReference ref = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser?.uid)
+      .collection('attendance');
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Attendance Report"),
-          backgroundColor: Colors.black,
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(),
-            ],
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text("Attendance Report"),
+        backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder<QuerySnapshot>(
+        future: ref.get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // ignore: prefer_is_empty
+            if (snapshot.data?.docs.length == 0) {
+              return const Center(
+                child: Text(
+                  "You have no attendance history to show !",
+                  style: TextStyle(
+                    color: Color.fromARGB(200, 255, 255, 255),
+                  ),
+                ),
+              );
+            }
+
+            return Scaffold(
+              body: Container(
+                margin: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 30,
+                ),
+                child: ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    // ignore: unnecessary_new, unused_local_variable
+                    Color bg = const Color.fromARGB(221, 55, 55, 55);
+                    Map? data = snapshot.data?.docs[index].data() as Map?;
+
+                    return InkWell(
+                      child: Card(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        color: bg,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "${data!['date']}",
+                                style: const TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(221, 255, 255, 255),
+                                ),
+                              ),
+                              //
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: Text("Loading..."),
+            );
+          }
+        },
+      ),
+    );
   }
 }
